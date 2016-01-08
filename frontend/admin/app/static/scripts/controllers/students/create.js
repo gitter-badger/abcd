af660be21a6ea56ada4c03da3a81ff53d36a2ccc
unknown <rafanetow@gmail.com>
2016-01-08 00:51:20 -0600

    working in  new controller to create students

---
+++ frontend/admin/app/static/scripts/controllers/students/create.js
@@ -0,0 +1,81 @@
+;(function(angular) {
+	'use strict';
+
+	angular.module('app.controllers').controller('CreateStudentCtrl', ['$scope', '$location', 'ngToast', 'Student',
+		function($scope, $location, ngToast, User) {
+			$scope.statuses = User.statuses;
+			$scope.passwordRepeat = '';
+			$scope.user = {
+				username: '',
+				password: '',
+				firstName: '',
+				lastName: '',
+				isAdmin: false,
+				isTeacher: false
+			};
+
+			function passwordsMatch() {
+				return $scope.user.password === $scope.passwordRepeat;
+			}
+
+			function setPasswordFieldsValidities(match) {
+				$scope.userForm.password.$setValidity('invalidPassword', match);
+				$scope.userForm.repeatPassword.$setValidity('invalidPassword', match);
+			}
+
+			$scope.onUsernameChange = function() {
+				if ($scope.user.username === '') {
+					$scope.userForm.username.$setValidity('available', true);
+					return;
+				}
+
+				User.findByUsername($scope.user.username).success(function() {
+					$scope.userForm.username.$setValidity('available', false);
+				}).error(function(response, status) {
+					$scope.userForm.username.$setValidity('available', status === 404);
+				});
+			};
+
+			$scope.onPasswordChange = function() {
+				var match = passwordsMatch();
+				setPasswordFieldsValidities(match);
+			};
+
+			$scope.createUser = function() {
+				var match = passwordsMatch();
+				setPasswordFieldsValidities(match);
+
+				if (!match) {
+					$scope.user.password = '';
+					$scope.passwordRepeat = '';
+					return;
+				}
+
+				User.create($scope.user).success(function(response) {
+					if (!response.success) {
+						ngToast.create({
+							className: 'danger',
+							content: response.errorMessage,
+							dismissButton: true
+						});
+						return;
+					}
+
+					ngToast.create('Se actualizó el usuario!');
+					$scope.userForm.$setPristine();
+					$scope.userForm.$setUntouched();
+					$scope.passwordRepeat = '';
+					$scope.user = {
+						username: '',
+						password: '',
+						firstName: '',
+						lastName: '',
+						email: '',
+						isAdmin: false,
+						isTeacher: false
+					};
+				});
+			};
+		}
+	]);
+})(angular);
